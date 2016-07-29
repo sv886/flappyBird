343633a9c4669d551b4a10917d50f3fcf58bb744
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-07-28 19:08:07 -0500

    preload pipe img, logic for 1 pipe and row of pipes

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var mainState = {
 
         // Load the bird sprite
         game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() {
@@ -32,6 +33,41 @@ var mainState = {
         var spaceKey = game.input.keyboard.addKey(
                         Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
+
+        // Create an empty group
+        this.pipes = game.add.group();
+
+        addOnePipe: function(x, y) {
+            // Create a pipe at the position x and y
+            var pipe = game.add.sprite(x, y, 'pipe');
+
+            // Add the pipe to our previously created group
+            this.pipes.add(pipe);
+
+            // Enable physics on the pipe
+            game.physics.arcade.enable(pipe);
+
+            // Add velocity to the pipe to make it move left
+            pipe.body.velocity.x = -200;
+
+            // Automatically kill the pipe when it's no longer visible
+            pipe.checkWorldBounds = true;
+            pipe.outOfBoundsKill = true;
+        },
+
+        addRowOfPipes: function() {
+            // Randomly pick a number between 1 and 5
+            // This will be the hole position
+            var hole = Math.floor(Math.random() * 5) + 1;
+
+            // Add the 6 pipes
+            // With one big hole at position 'hole' and 'hole + 1'
+            for (var i = 0; i < 8; i++)
+                if (i != hole && i != hole + 1)
+                    this.addOnePipe(400, i * 60 + 10);
+        },
+
+
     },
 
     update: function() {
